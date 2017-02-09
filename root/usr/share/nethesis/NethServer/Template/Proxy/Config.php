d37451ad236169bcc5aa7dfe7dc0ef583093d77f
Jacob Oliver <OSTechnology-Jacob@users.noreply.github.com>
2017-02-09 11:30:39 +0000

    Update Config.php
    Add ICAP Server properties

--- root/usr/share/nethesis/NethServer/Template/Proxy/Config.php
+++ root/usr/share/nethesis/NethServer/Template/Proxy/Config.php
@@ -19,12 +19,24 @@ echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $
 
 echo '<br>';
 
-$URL_Rewrite_config = $view->fieldSetSwitch('URL_Rewrite', 'enabled', $view::FIELDSETSWITCH_CHECKBOX)
-    ->setAttribute('template', $T('URL_Rewrite_label'))
+$URLRewrite_config = $view->fieldSetSwitch('URLRewrite_Config', 'enabled', $view::FIELDSETSWITCH_CHECKBOX)
+    ->setAttribute('template', $T('URLRewrite_label'))
     ->setAttribute('uncheckedValue', 'disabled')
-    ->insert($view->textInput('URL_Rewrite_Config')->setAttribute('placeholder', 'Test'))
+    ->insert($view->textInput('URLRewriteConfig')->setAttribute('placeholder', 'url_rewrite_program /usr/sbin/ufdbgclient -l /var/log/squid
+url_rewrite_children 20 startup=5 idle=5 concurrency=0
+url_rewrite_extras "%>a/%>A %un %>rm bump_mode=%ssl::bump_mode sni=\"%ssl::>sni\" referer=\"%{Referer}>h\""'))
     ->insert($view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP));
 
-echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('URL_Rewrite_label'))
-    ->insert($URL_Rewrite_config);
-    
+echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('URLRewrite_label'))
+    ->insert($URLRewrite_config);
+
+echo '<br/>';
+
+$ICAPServer_config = $view->fieldSetSwitch('ICAPServer_Config', 'enabled', $view::FIELDSETSWITCH_CHECKBOX)
+    ->setAttribute('template', $T('ICAPServer_label'))
+    ->setAttribute('uncheckedValue', 'disabled')
+    ->insert($view->textInput('ICAPServerConfig')->setAttribute('placeholder', 'Test'))
+    ->insert($view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP));
+
+echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('ICAPServer_label'))
+    ->insert($ICAPServer_config);
