39372bac5c060b2a038699e335685f6cbfc7f183
Jacob Oliver <OSTechnology-Jacob@users.noreply.github.com>
2017-02-09 11:14:44 +0000

    Recategorize and add URL Redirect category

--- root/usr/share/nethesis/NethServer/Template/Proxy/Config.php
+++ root/usr/share/nethesis/NethServer/Template/Proxy/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-echo $view->panel()
+$DiskCache_config = $view->panel()
     ->insert($view->radioButton('DiskCache', 'disabled')->setAttribute('label', $T('DiskCache_disabled_label')))
     ->insert($view->fieldsetSwitch('DiskCache', 'enabled', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('Webvirt_label'))
         ->insert($view->textInput('DiskCacheSize')->setAttribute('label', $T('DiskCacheSize_label'))
@@ -9,8 +9,22 @@ echo $view->panel()
         )
         ->insert($view->textInput('MaxObjSize')->setAttribute('label', $T('MaxObjSize_label'))
         )
+    )
+    ->insert($view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP)
+        ->insert($view->button('Clear_cache', $view::BUTTON_SUBMIT)->setAttribute('label', $T('ClearCache_label')))
     );
 
+echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('DiskCache_label'))
+    ->insert($DiskCache_config);
+
 echo '<br>';
-echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP)
-     ->insert($view->button('Clear_cache', $view::BUTTON_SUBMIT)->setAttribute('label', $T('ClearCache_label')));
+
+$URL_Rewrite_config = $view->fieldSetSwitch('URL_Rewrite', 'enabled', $view::FIELDSETSWITCH_CHECKBOX)
+    ->setAttribute('template', $T('URL_Rewrite_label'))
+    ->setAttribute('uncheckedValue', 'disabled')
+    ->insert($view->textInput('URL_Rewrite_Config')->setAttribute('placeholder', 'Test'))
+    ->insert($view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP));
+
+echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('URL_Rewrite_label'))
+    ->insert($URL_Rewrite_config);
+    
