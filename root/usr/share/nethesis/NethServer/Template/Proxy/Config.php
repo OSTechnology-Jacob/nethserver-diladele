5f2b6b1575c1319f36754cd72e65af33c27f5af8
Jacob Oliver <OSTechnology-Jacob@users.noreply.github.com>
2017-02-09 12:46:45 +0000

    Update Config.php

--- root/usr/share/nethesis/NethServer/Template/Proxy/Config.php
+++ root/usr/share/nethesis/NethServer/Template/Proxy/Config.php
@@ -1,15 +1,6 @@
 <?php
 
 $DiskCache_config = $view->panel()
-    ->insert($view->radioButton('DiskCache', 'disabled')->setAttribute('label', $T('DiskCache_disabled_label')))
-    ->insert($view->fieldsetSwitch('DiskCache', 'enabled', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('Webvirt_label'))
-        ->insert($view->textInput('DiskCacheSize')->setAttribute('label', $T('DiskCacheSize_label'))
-        )
-        ->insert($view->textInput('MinObjSize')->setAttribute('label', $T('MinObjSize_label'))
-        )
-        ->insert($view->textInput('MaxObjSize')->setAttribute('label', $T('MaxObjSize_label'))
-        )
-    )
     ->insert($view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP)
         ->insert($view->button('Clear_cache', $view::BUTTON_SUBMIT)->setAttribute('label', $T('ClearCache_label')))
     );
@@ -19,6 +10,7 @@ echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $
 
 echo '<br>';
 
+/* Disabled whilst under development
 $URLRewrite_config = $view->fieldSetSwitch('URLRewrite_Config', 'enabled', $view::FIELDSETSWITCH_CHECKBOX)
     ->setAttribute('template', $T('URLRewrite_label'))
     ->setAttribute('uncheckedValue', 'disabled')
@@ -40,3 +32,4 @@ $ICAPServer_config = $view->fieldSetSwitch('ICAPServer_Config', 'enabled', $view
 
 echo $view->fieldset('', $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('ICAPServer_label'))
     ->insert($ICAPServer_config);
+*/
