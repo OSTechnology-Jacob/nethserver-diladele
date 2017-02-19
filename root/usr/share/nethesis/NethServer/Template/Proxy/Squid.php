cd2fb7a6f0e3c7251576914f79ff86c8cdb14d6b
Jacob Oliver <OSTechnology-Jacob@users.noreply.github.com>
2017-02-19 22:53:32 +0000

    Update Squid.php

--- root/usr/share/nethesis/NethServer/Template/Proxy/Squid.php
+++ root/usr/share/nethesis/NethServer/Template/Proxy/Squid.php
@@ -1,17 +1,5 @@
 <?php
 
-$green_modes = $view->fieldset('GreenMode')->setAttribute('template', $T('GreenMode_label'))
-            ->insert($view->radioButton('GreenMode', 'manual'))
-            ->insert($view->radioButton('GreenMode', 'authenticated'))
-            ->insert($view->radioButton('GreenMode', 'transparent'))
-            ->insert($view->radioButton('GreenMode', 'transparent_ssl'));
-
-$blue_modes = $view->fieldset('BlueMode')->setAttribute('template', $T('BlueMode_label'))
-            ->insert($view->radioButton('BlueMode', 'manual'))
-            ->insert($view->radioButton('BlueMode', 'authenticated'))
-            ->insert($view->radioButton('BlueMode', 'transparent'))
-            ->insert($view->radioButton('BlueMode', 'transparent_ssl'));
-
 
 echo $view->fieldsetSwitch('status', 'enabled',  $view::FIELDSETSWITCH_CHECKBOX)
         ->setAttribute('template', $T('Squid_status'))
