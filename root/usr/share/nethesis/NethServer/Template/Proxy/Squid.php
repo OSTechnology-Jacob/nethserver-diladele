18bd20b34b660ddb33fd1bb33bdeb74d97dce71b
Jacob Oliver <OSTechnology-Jacob@users.noreply.github.com>
2017-02-19 23:11:00 +0000

    Update Squid.php

--- root/usr/share/nethesis/NethServer/Template/Proxy/Squid.php
+++ root/usr/share/nethesis/NethServer/Template/Proxy/Squid.php
@@ -1,12 +1,10 @@
 <?php
 
-echo '<br/><a href="/diladele" target="_blank" >Open Diladele management</a>';
-
 echo $view->fieldsetSwitch('status', 'enabled',  $view::FIELDSETSWITCH_CHECKBOX)
         ->setAttribute('template', $T('Squid_status'))
         ->setAttribute('uncheckedValue', 'disabled')
-        ->insert($green_modes)
-        ->insert($blue_modes)
         ->insert($view->checkbox('PortBlock', 'enabled')->setAttribute('uncheckedValue', 'disabled'));
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
+
+echo '<br/><a href="/diladele" target="_blank" >Open Diladele management</a>';
